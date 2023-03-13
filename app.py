45a21a092af929429319dafc88626ca9d35f519c
ArturLaptop <arturnovak128@gmail.com>
2023-03-13 15:02:42 +0100

    sekcja many to many relationships, polaczenie tagow z itemami

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import models
 
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
+from resources.tag import blp as TagBlueprint
 
 def create_app(db_url=None):
 
@@ -52,6 +53,7 @@ def create_app(db_url=None):
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
     
     return app
 
